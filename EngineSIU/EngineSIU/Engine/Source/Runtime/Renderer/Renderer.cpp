840189c57c97b6d9e53a420abb3c110217f22acc
Kyeongtaek Lee <skylove0911@naver.com>
2025-04-14 17:12:20 +0900

    [feat] UberShader Lighting 리팩토링

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/Renderer.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Renderer/Renderer.cpp
@@ -85,9 +85,6 @@ void FRenderer::CreateConstantBuffers()
     UINT lightingBufferSize = sizeof(FLightBuffer);
     BufferManager->CreateBufferGeneric<FLightBuffer>("FLightBuffer", nullptr, lightingBufferSize, D3D11_BIND_CONSTANT_BUFFER, D3D11_USAGE_DYNAMIC, D3D11_CPU_ACCESS_WRITE);
 
-    UINT litUnlitBufferSize = sizeof(FLitUnlitConstants);
-    BufferManager->CreateBufferGeneric<FLitUnlitConstants>("FLitUnlitConstants", nullptr, litUnlitBufferSize, D3D11_BIND_CONSTANT_BUFFER, D3D11_USAGE_DYNAMIC, D3D11_CPU_ACCESS_WRITE);
-
     UINT ScreenConstantsBufferSize = sizeof(FScreenConstants);
     BufferManager->CreateBufferGeneric<FScreenConstants>("FScreenConstants", nullptr, ScreenConstantsBufferSize, D3D11_BIND_CONSTANT_BUFFER, D3D11_USAGE_DYNAMIC, D3D11_CPU_ACCESS_WRITE);
 
